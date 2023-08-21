996ba82f07074bcbe88917eb7a3ee4b04223a3b3
Ahmed Elsankary <elsankary99@gmail.com>
2023-08-21 16:13:09 +0300

    onboarding part 2

---
+++ lib/core/constant/color.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color grey = Color(0xff5A5A5A);
+  static const Color white = Colors.white;
+}
--- lib/view/screen/onboarding.dart
+++ lib/view/screen/onboarding.dart
@@ -1,3 +1,5 @@
+import 'package:ecommerce/core/constant/color.dart';
+import 'package:ecommerce/data/datasource/static/static.dart';
 import 'package:flutter/material.dart';
 
 class OnBoarding extends StatelessWidget {
@@ -6,9 +8,33 @@ class OnBoarding extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColor.white,
       body: SafeArea(
-          child: Center(
-        child: Text('data'),
+          child: PageView.builder(
+        itemCount: onboardingList.length,
+        itemBuilder: (context, i) => Column(
+          children: [
+            const SizedBox(height: 30),
+            Text(
+              onboardingList[i].title!,
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 65),
+            Image.asset(
+              onboardingList[i].image!,
+              height: 250,
+              width: 200,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(height: 65),
+            Text(
+              onboardingList[i].body!,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                  height: 2, fontSize: 16, color: AppColor.grey),
+            ),
+          ],
+        ),
       )),
     );
   }

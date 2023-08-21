d3b84cb42ea93d3216a94cd6a1a91b8ef655d32a
Ahmed Elsankary <elsankary99@gmail.com>
2023-08-21 14:49:54 +0300

    onboarding part 1

---
+++ lib/view/screen/onboarding.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OnBoarding extends StatelessWidget {
+  const OnBoarding({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Center(
+        child: Text('data'),
+      )),
+    );
+  }
+}
